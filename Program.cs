9895e4a2798db45f3d8c8c9633617a2b47d18855
Ulrika77 <ulrika.karlsson@yahoo.se>
2020-11-03 15:27:09 +0100

    added sort function

--- Program.cs
+++ Program.cs
@@ -13,33 +13,35 @@ namespace LatinDictionary
             //declare variables
             string userInput = "";
             string[] word = { "vidare-se", "pater-far", "arbor-träd", "luna-måne", "movere-flytta", "urbs-stad" };
-            string[] menuArray = { "Show list", "Look up word in latin", "Exit" };            
+            string[] menuArray = { "Show list", "Look up word in latin", "Sort list", "Exit" };            
             int menuChoice = 0;
-            int[] menu = new int[] { 1, 2, 3 };          
+            int[] menu = new int[] { 1, 2, 3, 4 };
+            string wordInArray = "";
             Dictionary<string, string> latinDictionary = new Dictionary<string, string>();
             Console.WriteLine("**** Latin dictionary ****\n");
             bool quit = false;
             do
             {
                 Console.WriteLine();
-                for(int i = 0; i < 3; i++)
+                for(int i = 0; i < 4; i++)
                 { 
                     Console.WriteLine("{0}. {1}", menu[i], menuArray[i]);
                 }             
                 Console.Write("\nSelect an option from the meny:" );                           
                 userInput = Console.ReadLine();
                 menuChoice = int.Parse(userInput);
-                
-                if (menuChoice == 3)
+
+                if (menuChoice == 4)
                 {
                     quit = true;
-                }         
+                    break;
+                }
                 else if (menuChoice == 1)
-                {         
+                {
                     for (int i = 0; i < word.Length; i++)
                     {
                         Console.WriteLine(word[i]);
-                    }               
+                    }
                 }
                 else if (menuChoice == 2)
                 {
@@ -47,19 +49,29 @@ namespace LatinDictionary
                     userInput = Console.ReadLine();
                     for (int i = 0; i < word.Length; i++)
                     {
-                        string wordInArray = word[i];
+                        wordInArray = word[i];
                         string[] splitWord = wordInArray.Split('-');
                         if (splitWord[0] == userInput)
                         {
-                            Console.WriteLine("The translation of the word " + userInput + " is " + splitWord[1] + " in swedish");
+                            Console.WriteLine("The translation of the word '{0}' is '{1}' in swedish",userInput, splitWord[1]);
                         }
-                    }
+                    } 
+                }
+                else if (menuChoice == 3)
+                {
+                    for (int i = 0; i < word.Length; i++)
+                    {
+                        Array.Sort<string>(word);
+                        Console.WriteLine(word[i]);                                                                     
+                    }               
                 }
             }
             while (quit != true);
             {
                 Console.WriteLine("Have a nice day!");
             }
+            Console.WriteLine();
+            Console.ReadKey();
         }
     }
 }
