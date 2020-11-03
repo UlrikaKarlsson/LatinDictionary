7ff1a9263af69d6814244f177a41fcdf5987ca66
Ulrika77 <ulrika.karlsson@yahoo.se>
2020-11-03 14:48:47 +0100

    menu change, added search function

--- Program.cs
+++ Program.cs
@@ -12,30 +12,48 @@ namespace LatinDictionary
         {
             //declare variables
             string userInput = "";
-            int[] menu = new int[] { 1, 2 };
-            int menuChoice = 0, menu1 = 0, menu2 = 1;
+            string[] word = { "vidare-se", "pater-far", "arbor-träd", "luna-måne", "movere-flytta", "urbs-stad" };
+            string[] menuArray = { "Show list", "Look up word in latin", "Exit" };            
+            int menuChoice = 0;
+            int[] menu = new int[] { 1, 2, 3 };          
             Dictionary<string, string> latinDictionary = new Dictionary<string, string>();
             Console.WriteLine("**** Latin dictionary ****\n");
-
             bool quit = false;
-
             do
-            { 
-                Console.WriteLine("{0} Insert latin word", menu[menu1]);
-                Console.WriteLine("{0} Exit", menu[menu2]);
-                Console.Write("Select an option from the meny:" );            
-                
-               
+            {
+                Console.WriteLine();
+                for(int i = 0; i < 3; i++)
+                { 
+                    Console.WriteLine("{0}. {1}", menu[i], menuArray[i]);
+                }             
+                Console.Write("\nSelect an option from the meny:" );                           
                 userInput = Console.ReadLine();
                 menuChoice = int.Parse(userInput);
-
-                if (menuChoice == menu[menu2])
+                
+                if (menuChoice == 3)
                 {
                     quit = true;
+                }         
+                else if (menuChoice == 1)
+                {         
+                    for (int i = 0; i < word.Length; i++)
+                    {
+                        Console.WriteLine(word[i]);
+                    }               
                 }
-                else if (userInput == "new")
-                { 
-
+                else if (menuChoice == 2)
+                {
+                    Console.Write("What latin word do you want to translate into swedish? ");
+                    userInput = Console.ReadLine();
+                    for (int i = 0; i < word.Length; i++)
+                    {
+                        string wordInArray = word[i];
+                        string[] splitWord = wordInArray.Split('-');
+                        if (splitWord[0] == userInput)
+                        {
+                            Console.WriteLine("The translation of the word " + userInput + " is " + splitWord[1] + " in swedish");
+                        }
+                    }
                 }
             }
             while (quit != true);
