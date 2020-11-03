b451345c3fbfbb78311e721983c9a1e3ce5c8fae
Ulrika77 <ulrika.karlsson@yahoo.se>
2020-11-03 11:32:48 +0100

    added menu

--- Program.cs
+++ Program.cs
@@ -12,18 +12,34 @@ namespace LatinDictionary
         {
             //declare variables
             string userInput = "";
+            string[] menu = {"1. New latin word", "2. Quit"};
+
             Dictionary<string, string> latinDictionary = new Dictionary<string, string>();
-            Console.WriteLine("**** Latin dictionary, write quit to exit ****");
+            Console.WriteLine("**** Latin dictionary ****\n");
+
             bool quit = false;
 
             do
             {
-                Console.Write("Insert new latin word: ");
+                foreach(string s in menu)
+                {
+                    Console.WriteLine(s);
+                }
+                Console.Write("Select an option from the meny: ");
+
                 userInput = Console.ReadLine();
+                if (userInput == "quit")
+                {
+                    quit = true;
+                }
+                else if (userInput == "new")
+                { 
+
+                }
             }
             while (quit != true);
             {
-
+                Console.WriteLine("Have a nice day!");
             }
         }
     }
